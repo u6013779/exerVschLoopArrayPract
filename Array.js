276cf22f88d44155d5345a56d9a4f471012e7c13
u6013779 <u6013779@utah.edu>
2021-11-23 23:49:41 -0700

    noteasy

---
+++ Array.js
@@ -0,0 +1,40 @@
+var officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer",  "computer"]
+
+for (var i = 0; i < officeItems.length; i++) {
+    if (officeItems[i] == "computer") {
+        console.log(officeItems[i])
+    }
+}
+
+var peopleWhoWantToSeeMadMaxFuryRoad = [
+    {
+      name: "Mike",
+      age: 12,
+      gender: "male"
+    },{
+      name: "Madeline",
+      age: 80,
+      gender: "female"
+    },{
+      name: "Cheryl",
+      age: 22,
+      gender: "female"
+    },{
+      name: "Sam",
+      age: 30,
+      gender: "male"
+    },{
+      name: "Suzy",
+      age: 4,
+      gender: "female"
+    }
+]
+//options to use: function, .forthis
+function age(){
+    console.log( peopleWhoWantToSeeMadMaxFuryRoad.age )
+}
+
+
+[2, 5, 435, 4, 3] // "The light is on"
+[1, 1, 1, 1, 3]   // "The light is on"
+[9, 3, 4, 2]      // "The light is off"
